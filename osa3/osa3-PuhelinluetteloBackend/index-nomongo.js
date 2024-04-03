497fa0cac175ba6db0121afcadb6d30883958911
Miro Prusila <mprussa02@gmail.com>
2024-04-03 22:05:26 +0300

    Tehtävä 3.12

--- osa3/osa3-PuhelinluetteloBackend/index-nomongo.js
+++ osa3/osa3-PuhelinluetteloBackend/index-nomongo.js
@@ -1,11 +1,29 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const Person = require('./models/person')
 
 let persons = [
+    {
+      name: "Arto Hellas",
+      number: "040-123456",
+      id: 1
+    },
+    {
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+      id: 2
+    },
+    {
+      name: "Dan Abramov",
+      number: "12-43-234345",
+      id: 3
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: 4
+    }
 ]
 
 app.use(express.static('dist'))
@@ -20,14 +38,17 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)})
+    response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
         response.json(person)
-    })
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -63,7 +84,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
